7b5e1e9338dbf242bb98d4d13d616c043960e0ee
root <yahiyamohammed455@gmail.com>
2022-03-29 13:11:41 -0700

    Fixed chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -9,19 +9,19 @@
 
 void print_chessboard(char (*a)[8])
 {
-	int i = 0;
-	int j;
+	int i, j;
 
+	i = j = 0;
 	while (i < 8)
 	{
 		j = 0;
 		while (j < 8)
 		{
 			_putchar(a[i][j]);
-			if (j == 7)
-				_putchar('\n');
 			j++;
 		}
+		_putchar('\n');
 		i++;
 	}
+
 }
