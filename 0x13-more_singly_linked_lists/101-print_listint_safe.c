7ca68319f9bc86f9a5208ff8fa1da1cbfceb63ee
root <yahiyamohammed455@gmail.com>
2022-04-26 14:01:35 -0700

    G

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,30 +1,50 @@
 #include "lists.h"
 
 /**
- * print_listint_safe - prints list, avoids loop
- * @head: points to start of list
- * Return: number of nodes in size_t or exit 98
+ * print_listint_safe - Function that prints a linked list
+ * @head: Beginning of the list
+ * Return: Number of nodes
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	/* declarations */
-	size_t nodeCount = 0;
-	/* check for null pointer */
-	while (head)
-	{
-		printf("[%p] %d\n", (void *)head, head->n);
-		nodeCount += 1;
+	unsigned int i = 0, j = 0, k = 0, bool = 0;
+	listint_t *slow, *fast, *temp;
 
-		if (head > head->next)
+	if (head == NULL)
+		printf("0\n"), exit(98);
+	slow = (listint_t *)head, fast = (listint_t *)head;
+	while (slow && fast && fast->next)
+	{
+		slow = slow->next, fast = fast->next->next;
+		if (slow == fast)
 		{
-			head = head->next;
-		}
-		else
-		{
-			head = head->next;
-			printf("-> [%p] %d\n", (void *)head, head->n);
+			bool = 1;
 			break;
 		}
 	}
-	return (nodeCount);
+	if (bool == 1)
+	{
+		for (j = 1; fast->next != slow; j++)
+			fast = fast->next;
+		slow = (listint_t *)head, fast = (listint_t *)head;
+		for (; k < j; k++)
+			fast = fast->next;
+		while (1)
+		{
+			fast = fast->next, slow = slow->next;
+			if (fast->next == slow->next)
+			{
+				temp = slow->next, fast->next = NULL;
+				break;
+			}
+		}
+	}
+	while (head)
+	{
+		(head == NULL) ? (void)exit(98) : (void)head;
+		printf("[%p] %d\n", (void *)head, head->n);
+		head = head->next, i++;
+	}
+	(bool == 1) ? printf("-> [%p] %d\n", (void *)temp, temp->n) : (int)bool;
+	return (i);
 }
